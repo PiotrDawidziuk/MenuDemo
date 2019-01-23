6a81f99f7221711f88f0722c16f5df2cef496b05
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-01-23 16:58:11 +0100

    menu selected addedd

--- app/src/main/java/pl/apka/menudemo/MainActivity.java
+++ app/src/main/java/pl/apka/menudemo/MainActivity.java
@@ -2,8 +2,30 @@ package pl.apka.menudemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        
+
+         super.onOptionsItemSelected(item);
+
+         return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
